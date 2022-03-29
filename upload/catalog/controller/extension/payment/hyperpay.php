406c3a91535f6cc23338e9e8c8729545cfa529a1
ahmada3mar <ahmad.aimer@hyperpay.com>
2022-03-29 13:29:36 +0300

    amount fix

--- upload/catalog/controller/extension/payment/hyperpay.php
+++ upload/catalog/controller/extension/payment/hyperpay.php
@@ -30,7 +30,7 @@ class ControllerExtensionPaymentHyperpay extends Controller
         $token = $this->config->get('payment_hyperpay_accesstoken');
         $type = $this->config->get('payment_hyperpay_trans_type');
         $connector = $this->config->get('payment_hyperpay_connector');
-        $amount = number_format(round($orderAmount, 2), 2, '.', '');
+        $amount = number_format($orderAmount * $order_info['currency_value'] ,2, '.', '');
         $currency = $this->config->get('payment_hyperpay_base_currency');
         $transactionID = $orderid;
         $firstName = $order_info['payment_firstname'];
