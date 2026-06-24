b633a322b38e773f687d51e6481c0478c4f98de2
Alrawajih <ali.alrawajih@hyperpay.com>
2026-06-24 09:47:16 +0300

    Enable JAYWAN Payment Brand

--- upload/catalog/model/extension/payment/hyperpay_valu.php
+++ upload/catalog/model/extension/payment/hyperpay_valu.php
@@ -10,6 +10,7 @@ class ModelExtensionPaymentHyperpayValu extends Model {
             'terms'      => '',
             'title'      => $this->config->get('payment_hyperpay_valu_heading_title'),
             'sort_order' => $this->config->get('payment_hyperpay_valu_sort_order'),
+            'icon' => HTTPS_SERVER . 'image/catalog/hyperpay/VALU.svg'
         );
 
         return $method_data;
